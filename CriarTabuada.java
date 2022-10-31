f3c0e8525339e0dbaf7d755d3dbb120d75857367
Alan Dias da Silva <79154696+diasprogramacao@users.noreply.github.com>
2022-10-31 19:26:36 -0300

    CriarTabuada
    CriarTabuada

---
+++ CriarTabuada.java
@@ -0,0 +1,12 @@
+
+public class CriarTabuada {
+	
+	public static void main(String[]agrs) {
+		
+		Tabuada primeira = new Tabuada();
+		primeira.setNumeroTabuada(2);
+		primeira.gerarTabuadaCompleta();
+		
+	}
+
+}
